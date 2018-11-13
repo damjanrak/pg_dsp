79975015ee18737eeea39cdd01fa5b338ab0d566
drakanovic <damjan.rakanovic-ext@frobas.com>
2018-11-13 13:44:48 +0100

    Retry with quickstart

--- docs/conf.py
+++ docs/conf.py
@@ -1,38 +1,45 @@
-import pkg_resources
-import datetime
+# -*- coding: utf-8 -*-
+#
+# Configuration file for the Sphinx documentation builder.
+#
+# This file does only contain a selection of the most common options. For a
+# full list see the documentation:
+# http://www.sphinx-doc.org/en/master/config
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+
 
 # -- Project information -----------------------------------------------------
 
 project = 'pg_dsp'
-this_year = datetime.date.today().year
-copyright = f'{this_year}, Damjan Rakanovic'
+copyright = '2018, Damjan Rakanovic'
 author = 'Damjan Rakanovic'
 
-version = pkg_resources.get_distribution("pg_dsp").version
-release = version
+# The short X.Y version
+version = ''
+# The full version, including alpha/beta/rc tags
+release = ''
 
-# extensions = [
-#     'sphinx.ext.autodoc', 'sphinx.ext.githubpages', 'sphinxcontrib.tikz',
-#     'sphinxcontrib.wavedrom', 'sphinx.ext.napoleon',
-#     'sphinx_sitemap', 'sphinx.ext.autosectionlabel'
-# ]
 
-autosectionlabel_prefix_document = True
+# -- General configuration ---------------------------------------------------
 
-# site_url = "https://www.pygears.org/"
+# If your documentation needs a minimal Sphinx version, state it here.
+#
+# needs_sphinx = '1.0'
 
-autodoc_default_options = {
-    'show-inheritance': None,
-    'members': None,
-    'no-special-members': None,
-    'exclude-members': 'mro, __weakref__,  __new__, __str__, __repr__',
-}
-autoclass_content = "class"
-add_module_names = False
-
-offline_skin_js_path = r"default.js"
-offline_wavedrom_js_path = r"wavedrom.js"
-
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -55,11 +62,12 @@ language = None
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path .
+# This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = None
+
 
 # -- Options for HTML output -------------------------------------------------
 
@@ -67,38 +75,34 @@ pygments_style = 'sphinx'
 # a list of builtin themes.
 #
 html_theme = 'alabaster'
-# html_title = "PyGears - HW Design: A Functional Approach"
-# html_short_title = "PyGears"
-# html_baseurl = "https://www.pygears.org"
-# html_favicon = '_static/pygears.ico'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-html_theme_options = {
-    'travis_button': False,
-    'github_user': 'damjanrak',
-    'github_repo': 'pg_dsp',
-    'github_banner': True,
-    'description': 'HW Design: A Functional Approach',
-    # 'logo': 'logo.png',
-    # 'sidebar_width': '230px'
-}
+# html_theme_options = {}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
+html_static_path = ['_static']
 
-# html_sidebars = {
-#     '**': ['about.html', 'blog_link.html', 'globaltoc.html', 'searchbox.html']
-# }
+# Custom sidebar templates, must be a dictionary that maps document names
+# to template names.
+#
+# The default sidebars (for documents that don't match any pattern) are
+# defined by theme itself.  Builtin themes are using these templates by
+# default: ``['localtoc.html', 'relations.html', 'sourcelink.html',
+# 'searchbox.html']``.
+#
+# html_sidebars = {}
+
 
 # -- Options for HTMLHelp output ---------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'pygearsdoc'
+htmlhelp_basename = 'pg_dspdoc'
+
 
 # -- Options for LaTeX output ------------------------------------------------
 
@@ -113,8 +117,7 @@ latex_elements = {
 
     # Additional stuff for the LaTeX preamble.
     #
-    'preamble': r'''
-    ''',
+    # 'preamble': '',
 
     # Latex figure (float) alignment
     #
@@ -125,15 +128,20 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'pygears.tex', 'pygears Documentation', 'Damjan Rakanovic',
-     'manual'),
+    (master_doc, 'pg_dsp.tex', 'pg\\_dsp Documentation',
+     'Damjan Rakanovic', 'manual'),
 ]
+
 
 # -- Options for manual page output ------------------------------------------
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
-man_pages = [(master_doc, 'pygears', 'pygears Documentation', [author], 1)]
+man_pages = [
+    (master_doc, 'pg_dsp', 'pg_dsp Documentation',
+     [author], 1)
+]
+
 
 # -- Options for Texinfo output ----------------------------------------------
 
@@ -141,6 +149,25 @@ man_pages = [(master_doc, 'pygears', 'pygears Documentation', [author], 1)]
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'pygears', 'pygears Documentation', author, 'pygears',
-     'One line description of project.', 'Miscellaneous'),
+    (master_doc, 'pg_dsp', 'pg_dsp Documentation',
+     author, 'pg_dsp', 'One line description of project.',
+     'Miscellaneous'),
 ]
+
+
+# -- Options for Epub output -------------------------------------------------
+
+# Bibliographic Dublin Core info.
+epub_title = project
+
+# The unique identifier of the text. This can be a ISBN number
+# or the project homepage.
+#
+# epub_identifier = ''
+
+# A unique identification for the text.
+#
+# epub_uid = ''
+
+# A list of files that should not be packed into the epub file.
+epub_exclude_files = ['search.html']
